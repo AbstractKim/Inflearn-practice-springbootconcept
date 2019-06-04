f543b50b89a67fe15cc14e6ae6be7dc8c6616cd4
peter <b.kim@spaceplam.com>
2019-06-04 01:02:17 -0400

    create custom autoconfiguration 2 - rectifying

--- SpringBootStart/src/main/java/com/github/abstarctkim/Application.java
+++ SpringBootStart/src/main/java/com/github/abstarctkim/Application.java
@@ -14,11 +14,11 @@ public class Application {
         application.run(args);
     }
 
-    @Bean
-    public Peter peter(){
-        Peter peter = new Peter();
-        peter.setName("B");
-        peter.setAge(20);
-        return peter;
-    }
+//    @Bean
+//    public Peter peter(){
+//        Peter peter = new Peter();
+//        peter.setName("B");
+//        peter.setAge(20);
+//        return peter;
+//    }
 }
